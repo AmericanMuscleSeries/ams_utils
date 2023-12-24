757662f8bfb0fe40a74d00848f49d87d254e0b86
FinniMcFinger <rrmcgrew@gmail.com>
2023-12-24 00:57:23 -0600

    add division entry field to registration modal

--- discord-bot/registration.py
+++ discord-bot/registration.py
@@ -1,10 +1,14 @@
+import constants as const
 import discord
 import json
 import logging
 
+from discord import utils
+
 
 log = logging.getLogger('discord')
 _users = 'static/data/users.json'
+_divs = ['PRO', 'AM']
 
 
 class RegistrationModal(discord.ui.Modal, title='AMS Registration'):
@@ -12,6 +16,8 @@ class RegistrationModal(discord.ui.Modal, title='AMS Registration'):
     _pref_name_ = discord.ui.TextInput(label='What is your preferred full name?', placeholder='Marty McFly', required=True, style=discord.TextStyle.short)
     _team_ = discord.ui.TextInput(label='What team will you race for? (optional)', placeholder='Privateer', required=False, style=discord.TextStyle.short)
     _loc_ = discord.ui.TextInput(label='Where are you located? (optional)', placeholder='Parts Unknown', required=False, style=discord.TextStyle.short)
+    _div_ = discord.ui.TextInput(label='Which division are you entering? (PRO or AM)', placeholder='PRO or AM, IR > 2500 must be PRO', required=True,
+                                 style=discord.TextStyle.short)
 
 
     def read_users(self):
@@ -41,6 +47,14 @@ class RegistrationModal(discord.ui.Modal, title='AMS Registration'):
             users[user_]['pref_name'] = str(self._pref_name_)
             users[user_]['team'] = str(self._team_)
             users[user_]['loc'] = str(self._loc_)
+            users[user_]['div'] = str(self._div_).upper()
             log.info(f'registration received: {users[user_]}')
             self.write_users(users)
+            await self.verify_div(str(self._div_), interaction)
             await interaction.response.send_message('You are now registered.', ephemeral=True)
+    
+
+    async def verify_div(self, div: str, interaction: discord.Interaction):
+        if div.upper not in _divs:
+            admin_ch = utils.get(interaction.guild.channels, name=const.ADMIN_CH)
+            await admin_ch.send(f'Invalid division entered for {interaction.user.id} ({interaction.user.display_name}): {div.upper()}')
