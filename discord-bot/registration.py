ec57fcdf2bcd796a254916fdf38dda3a87b34900
FinniMcFinger <rrmcgrew@gmail.com>
2024-06-02 13:04:53 -0500

    adds vefirication to division selection

--- discord-bot/registration.py
+++ discord-bot/registration.py
@@ -19,34 +19,40 @@ class RegistrationModal(discord.ui.Modal, title='AMS Registration'):
 
 
     async def on_submit(self, interaction: discord.Interaction):
-        rules = 'https://discord.com/channels/916828519487656007/1025052192815718430/1193226323791990784'
-        payment = 'https://discord.com/channels/916828519487656007/1025052192815718430/1193304565697740860'
-        users = utils.read_json_file(_users)
-        user_ = str(interaction.user.id)
-        users[user_] = {}
-        users[user_]['iracing_id'] = str(self._ir_id_)
-        users[user_]['pref_name'] = str(self._pref_name_)
-        users[user_]['team'] = str(self._team_)
-        users[user_]['loc'] = str(self._loc_)
-        users[user_]['div'] = str(self._div_).upper()
-        users[user_]['num'] = '0'
-        log.info(f'registration received: {users[user_]}')
-        await self.verify_div(str(self._div_), interaction)
-        await self.set_role(str(self._div_), interaction.user, interaction)
-        await utils.set_nick(interaction, str(self._pref_name_))
-        utils.write_json_file(users,_users)
-        await utils.admin_log(interaction.guild, f'/register called by {interaction.user.name}')
-        await interaction.response.send_message(f'You are now on the registered. Please read the rules {rules}. '
-                                                f'You can modify your registration details via commands. '
-                                                f'If you are PRO or CH, use /number to select your number. '
-                                                f'You can see payment info for your entry fee here: {payment}. '
-                                                f'Use `/help` to see available commands.', ephemeral=True)
+        if good_div:
+            rules = 'https://discord.com/channels/916828519487656007/1025052192815718430/1193226323791990784'
+            payment = 'https://discord.com/channels/916828519487656007/1025052192815718430/1193304565697740860'
+            users = utils.read_json_file(_users)
+            user_ = str(interaction.user.id)
+            users[user_] = {}
+            users[user_]['iracing_id'] = str(self._ir_id_)
+            users[user_]['pref_name'] = str(self._pref_name_)
+            users[user_]['team'] = str(self._team_)
+            users[user_]['loc'] = str(self._loc_)
+            users[user_]['div'] = str(self._div_).upper()
+            users[user_]['num'] = '0'
+            log.info(f'registration received: {users[user_]}')
+            good_div = await self.verify_div(str(self._div_), interaction)        
+            await self.set_role(str(self._div_), interaction.user, interaction)
+            await utils.set_nick(interaction, str(self._pref_name_))
+            utils.write_json_file(users,_users)
+            await utils.admin_log(interaction.guild, f'/register called by {interaction.user.name}')
+            await interaction.response.send_message(f'You are now on the registered. Please read the rules {rules}. '
+                                                    f'You can modify your registration details via commands. '
+                                                    f'If you are PRO or CH, use /number to select your number. '
+                                                    f'You can see payment info for your entry fee here: {payment}. '
+                                                    f'Use `/help` to see available commands.', ephemeral=True)
+        else:
+            await interaction.response.send_message(f'Please try again and only enter PRO, CH, or AM in the division space. You entered: {self._div_}')
     
 
-    async def verify_div(self, div: str, interaction: discord.Interaction):
+    async def verify_div(self, div: str, interaction: discord.Interaction) -> bool:
         if div.upper() not in _divs:
             admin_ch = discord.utils.get(interaction.guild.channels, name=const.ADMIN_CH)
             await admin_ch.send(f'Invalid division entered for {interaction.user.id} ({interaction.user.display_name}): {div.upper()}')
+            return False
+        else:
+            return True
     
     
     async def set_role(self, div: str, member: discord.Member, interaction: discord.Interaction):
