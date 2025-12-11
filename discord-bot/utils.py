cfe9900a0f72f23d8a04e3f51722e3264e480f5d
FinniMcFinger <rrmcgrew@gmail.com>
2025-12-10 23:19:09 -0600

    adds Randy to admonishment

--- discord-bot/utils.py
+++ discord-bot/utils.py
@@ -13,7 +13,7 @@ def is_admin(id: int) -> bool:
 
 
 async def admonish(interaction: discord.Interaction) -> None:
-    await interaction.response.send_message('Hey punk, you\'re not an admin!')
+    await interaction.response.send_message('Hey punk, you\'re not an admin! Frig off!')
 
 
 def read_json_file(file_path: str) -> dict:
